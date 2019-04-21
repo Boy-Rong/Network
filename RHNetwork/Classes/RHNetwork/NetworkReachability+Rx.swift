95be09950fc3910e9fdf3afd3de7955aef824c27
rongheng <495929699g@gmail.com>
2019-04-21 15:19:35 +0800

    1.删除网络可用性的RX扩展，为了减少依赖

--- RHNetwork/Classes/RHNetwork/NetworkReachability+Rx.swift
+++ RHNetwork/Classes/RHNetwork/NetworkReachability+Rx.swift
@@ -5,31 +5,30 @@
 //  Created by 荣恒 on 2019/4/19.
 //
 
-import RxSwift
-import RxCocoa
-
-extension NetworkReachabilityService : ReactiveCompatible {}
-
-public extension Reactive where Base == NetworkReachabilityService {
-    
-    /// 网路状态序列
-    var status : Observable<NetworkReachabilityStatus> {
-        return NotificationCenter.default.rx.notification(.reachabilityChanged)
-            .flatMap({ notification -> Observable<NetworkReachabilityStatus> in
-                if let status = notification.userInfo?[NetworkKey.reachabilityChanged] as? NetworkReachabilityStatus {
-                    return Observable.just(status)
-                } else {
-                    return Observable.empty()
-                }
-            }).distinctUntilChanged()
-    }
-    
-    /// 是否有网
-    var isHasNetwork : Observable<Bool> {
-        return status.map({ value in
-            let values : [NetworkReachabilityStatus] = [.notReachable,.unknown]
-            return !values.contains(value)
-        })
-    } 
-    
-}
+//import RxSwift
+//
+//extension NetworkReachabilityService : ReactiveCompatible {}
+//
+//public extension Reactive where Base == NetworkReachabilityService {
+//
+//    /// 网路状态序列
+//    var status : Observable<NetworkReachabilityStatus> {
+//        return NotificationCenter.default.rx.notification(.reachabilityChanged)
+//            .flatMap({ notification -> Observable<NetworkReachabilityStatus> in
+//                if let status = notification.userInfo?[NetworkKey.reachabilityChanged] as? NetworkReachabilityStatus {
+//                    return Observable.just(status)
+//                } else {
+//                    return Observable.empty()
+//                }
+//            }).distinctUntilChanged()
+//    }
+//
+//    /// 是否有网
+//    var isHasNetwork : Observable<Bool> {
+//        return status.map({ value in
+//            let values : [NetworkReachabilityStatus] = [.notReachable,.unknown]
+//            return !values.contains(value)
+//        })
+//    }
+//
+//}

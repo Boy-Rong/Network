399e1f97f60951b0fe4bf14c9a6b6d9eecbcdbc9
rongheng <495929699g@gmail.com>
2019-07-11 20:43:15 +0800

    1.保存代码

--- Example/RHNetwork/ViewController.swift
+++ Example/RHNetwork/ViewController.swift
@@ -22,3 +22,48 @@ class ViewController: UIViewController {
 
 }
 
+
+/**
+ import RxSwift
+ import RxSwiftExtensions
+ import Network
+ 
+ func jr_page<RequestParams,Result,Value,NewValue>(
+ frist: Observable<Void>,
+ next: Observable<Void>,
+ params: Observable<RequestParams>,
+ requestFromParams: @escaping (RequestParams,Int) -> Observable<Result>,
+ valuesFromResult: @escaping (Result) -> (Value),
+ transform: @escaping (Value) -> (NewValue))
+ ->
+ (values: Observable<[NewValue]>,
+ isLoading: Observable<Bool>,
+ total: Observable<Int>) {
+ var page = 1
+ let isRefresh = Observable.merge(frist.mapValue(true), next.mapValue(false)
+ ).startWith(true)
+ let requestPage = isRefresh.map { $0 ? 1 : (page + 1) }
+ let loadState = PublishSubject<PageLoadState>()
+ let error = PublishSubject<NetworkError>()
+ //let indicator = ActivityIndicator()
+ frist.flatMap {_ in
+ next.startWith(())
+ .withLatestFrom(params, requestPage)
+ .flatMapLatest({ (_params,_page) -> Observable<NewValue> in
+ return requestFromParams(_params, _page)
+ .shareOnce()
+ .doNext {
+ 
+ }
+ .observeOn(transformScheduler)
+ .map(valuesFromResult)
+ .map(transform)
+ })
+ }
+ 
+ }
+ 
+ 
+ private let transformScheduler = ConcurrentDispatchQueueScheduler(qos: .default)
+
+ */
